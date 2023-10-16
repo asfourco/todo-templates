b8d96a9219b2d2755e20cfc6020b6bc4317ff293
Fadi Asfour <fadi.asfour@gmail.com>
2023-10-16 11:14:29 -0700

    fix dockerization of frontend

--- frontend/src/services/TodoService.ts
+++ frontend/src/services/TodoService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {Todo} from "../models/Todo";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API_URL = `${BACKEND_URL}/api/v1/todos`;
 
 export const getTodos = async (page: number, pageSize: number):Promise<Todo[]> =>
